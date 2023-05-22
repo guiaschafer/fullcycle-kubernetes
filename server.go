01a46a026c2155bf8c1c7d96472318fd5ff4a51d
Guilherme Schafer <guilherme.a.schafer@aubay.pt>
2023-05-22 08:12:33 +0100

    fullcycle - kubernetes - working with readiness and startupProbe

--- server.go
+++ server.go
@@ -45,7 +45,7 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	var duration = time.Since(startedAt)
 
-	if duration.Seconds() > 25 {
+	if duration.Seconds() < 10 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
 	} else {
